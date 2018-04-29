8cac41fb4de29ee4beed16eafa63c6c6c4e9a272
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-04-29 13:22:03 +0300

    More additions

---
+++ CPAP/usenet.py
@@ -0,0 +1,41 @@
+import datetime
+import nntplib
+import threading
+
+from nntplib import NNTP
+
+
+servers = [
+    'news.alaska-software.com',
+    'news.annexcafe.com',
+    'news.asp-shareware.org',
+    'news.1000years.com'
+]
+
+
+def main_func(year):
+    nntp = NNTP('news.alaska-software.com')
+    print(year)
+    for month in range(1, 12):
+        for day in range(1, 28):
+            date = datetime.date(year, month, day)
+            groups = nntp.newgroups(date)[1]
+            for group in groups:
+                try:
+                    news = nntp.newnews(group, date)
+                except nntplib.NNTPPermanentError:
+                    continue
+                else:
+                    print(news)
+
+
+threads = []
+
+for year in range(2005, 2018):
+    threads.append(threading.Thread(target=main_func, args=(year,)))
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
