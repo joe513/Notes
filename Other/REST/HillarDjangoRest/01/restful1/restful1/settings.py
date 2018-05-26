9e5af2a69e5f18c0cb19123c7094f60289efffbd
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-05-26 19:40:30 +0300

    Added PostgreSQL database managements system to the project, added new 'drones' app, made unabled toys app.

--- Other/REST/HillarDjangoRest/01/restful1/restful1/settings.py
+++ Other/REST/HillarDjangoRest/01/restful1/restful1/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'toys.apps.ToysConfig'
+    'drones.apps.DronesConfig'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'restful1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drones',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
